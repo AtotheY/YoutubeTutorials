22f109a439842c67b7980c43cb850ae7b90bd506
AtotheY <sistillianthony@gmail.com>
2018-04-15 12:04:46 -0400

    Added delete code.

--- Introductions/linkedListOnlyNodes.py
+++ Introductions/linkedListOnlyNodes.py
@@ -19,7 +19,43 @@ node2.nextNode = node3 # node2 -> node3 , "7" -> "10"
 
 # node1 -> node2 -> node3
 
-currentNode = node1
+head = node1
+print "*********************************"
+print "Traversing the regular linkedList"
+print "*********************************"
+# Regular Traversal
+currentNode = head
+while currentNode is not None:
+    print currentNode.value,
+    currentNode = currentNode.nextNode
+print ''
+print "*********************************"
+
+
+def deleteNode(head, valueToDelete):
+    currentNode = head
+    previousNode = None
+    while currentNode is not None:
+        if currentNode.value == valueToDelete:
+            if previousNode is None:
+                newHead = currentNode.nextNode
+                currentNode.nextNode = None
+                return newHead
+            previousNode.nextNode = currentNode.nextNode
+            return head
+        previousNode = currentNode
+        currentNode = currentNode.nextNode
+    return head # Value to delete was not found.
+
+print "deleting the node '7'"
+newHead = deleteNode(head, "0")
+
+
+print "*********************************"
+print "traversing the new linkedList with the node 7 removed"
+print "*********************************"
+
+currentNode = newHead
 while currentNode is not None:
     print currentNode.value,
     currentNode = currentNode.nextNode
